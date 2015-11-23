58291420de9640d08566de53dd9430eed880d0f8
timkg <tim.theodor@gmail.com>
2015-11-23 20:29:04 +0100

    adds start instructions and express static file server

---
+++ README.md
@@ -0,0 +1,9 @@
+Please run:
+
+```
+npm install
+npm run build
+npm start
+```
+
+And point two different browser windows or tabs to `localhost:3000`.
--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,18 @@ require("dotenv").load();
 var Server = require("socket.io");
 var makeStore = require("./store");
 var store = makeStore();
+var express = require("express");
+var app = express();
+
+app.use(express.static("dist"));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/dist/index.html");
+});
+
+var HTTP_PORT = process.env.HTTP_PORT || 3000;
+app.listen(HTTP_PORT, function () {
+  console.log("Server listening on port %s", HTTP_PORT);
+});
 
 var WEBSOCKET_PORT = process.env.WEBSOCKET_PORT || 8090;
 var io = new Server().attach(WEBSOCKET_PORT);
